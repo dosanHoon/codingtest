af9bfb4d053aefdcaebdce4bc7be34ab10450b8b
danedehaan <danedehaan@qubitsec.com>
2020-07-06 13:40:50 +0900

    3위까지 합산

--- 반장선거/반장선거.js
+++ 반장선거/반장선거.js
@@ -1,13 +1,12 @@
 export default function 반장선거(후보들) {
-  const 후보들arr = 후보들.split(" ");
-  const 결과합산 = {};
-  후보들arr.forEach((후보) => {
-    if (결과합산[후보]) {
-      ++결과합산[후보];
+  const 결과합산 = 후보들.split(" ").reduce((합산, 후보) => {
+    if (합산[후보]) {
+      ++합산[후보];
     } else {
-      결과합산[후보] = 1;
+      합산[후보] = 1;
     }
-  });
+    return 합산;
+  }, {});
 
   const 최종당선후보 = Object.keys(결과합산).reduce((당선후보, 현재후보) => {
     return 결과합산[현재후보] > 결과합산[당선후보] ? 현재후보 : 당선후보;
