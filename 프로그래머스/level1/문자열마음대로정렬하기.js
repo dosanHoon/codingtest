56b6117826a0f193d0ab1418afcc18c232185ab3
danedehaan <danedehaan@qubitsec.com>
2020-08-19 15:14:55 +0900

    문자열마음대로정렬하기

---
+++ 프로그래머스/level1/문자열마음대로정렬하기.js
@@ -0,0 +1,14 @@
+// 문자열 내 마음대로 정렬하기
+// [https://programmers.co.kr/learn/courses/30/lessons/12915]출처
+
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1의 문자 u, e, a로 strings를 정렬합니다.
+
+export default function solution(strings, n) {
+  return strings.sort((a, b) => {
+    if (a[n] > b[n] || (a[n] === b[n] && a > b)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
