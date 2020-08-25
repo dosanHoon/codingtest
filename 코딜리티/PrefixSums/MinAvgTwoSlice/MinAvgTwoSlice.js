fb521cfe3e555fcc28afafd50585747c8ee5e5a3
dosanhoon <hangru1106@gmail.com>
2020-08-25 11:31:37 +0900

    MinAvgTwoSlice 정답풀이

--- 코딜리티/PrefixSums/MinAvgTwoSlice/MinAvgTwoSlice.js
+++ 코딜리티/PrefixSums/MinAvgTwoSlice/MinAvgTwoSlice.js
@@ -1,7 +1,4 @@
 function solution(A) {
-  if (A.length === 2) {
-    return 0;
-  }
   let minIndex = 0;
   let minAvg = A[0] + A[1] / 2;
 
@@ -18,6 +15,27 @@ function solution(A) {
   }
 
   return minIndex;
+}
+
+function solution(A) {
+  let min = (A[0] + A[1]) / 2;
+  let minIdx = 0;
+  for (let i = 1; i < A.length - 1; i++) {
+    let two = (A[i] + A[i + 1]) / 2;
+    if (i > A.length - 2) {
+      if (two < min) {
+        min = two;
+        minIdx = i;
+      }
+    } else {
+      let three = (A[i] + A[i + 1] + A[i + 2]) / 3;
+      if (two < min || three < min) {
+        min = two < three ? two : three;
+        minIdx = i;
+      }
+    }
+  }
+  return minIdx;
 }
 
 export default solution;
