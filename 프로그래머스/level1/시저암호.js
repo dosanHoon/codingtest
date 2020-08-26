9a67d4dad1e10d383f4587ca4ef62cb6832f9758
dosanhoon <hangru1106@gmail.com>
2020-08-26 09:59:57 +0900

    프로그래머스 시저암호

---
+++ 프로그래머스/level1/시저암호.js
@@ -0,0 +1,14 @@
+function solution(s, n) {
+  return s
+    .split("")
+    .map((c) => {
+      if (c === " ") return " ";
+      const isUpletter = c.toUpperCase() === c;
+      let code = c.charCodeAt() + n;
+      if ((isUpletter && code > 90) || (!isUpletter && code > 122)) {
+        code -= 26;
+      }
+      return String.fromCharCode(code);
+    })
+    .join("");
+}
